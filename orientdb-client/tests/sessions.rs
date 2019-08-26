98eb06ae7bcfa890049374d2c874345a957ab295
wolf4ood <enrico.risa@gmail.com>
2019-08-26 22:19:24 +0200

    added API client::sessions

--- orientdb-client/tests/sessions.rs
+++ orientdb-client/tests/sessions.rs
@@ -31,3 +31,44 @@ fn test_open_sessions() {
 
     assert_eq!(20, pool.idle());
 }
+
+#[cfg(feature = "async")]
+mod asynchronous {
+
+    use super::common::asynchronous::{connect, create_database};
+    use super::config;
+
+    use async_std::task::block_on;
+
+    #[test]
+    fn test_open_sessions() {
+        block_on(async {
+            let client = connect().await;
+            let config = config();
+
+            create_database("async_test_open_sessions", &client, &config).await;
+
+            let result = client
+                .sessions(
+                    "async_test_open_sessions",
+                    &config.username,
+                    &config.password,
+                    None,
+                    Some(20),
+                )
+                .await;
+
+            assert!(result.is_ok());
+
+            let pool = result.unwrap();
+
+            let session = pool.get().await.unwrap();
+
+            assert_eq!(19, pool.idle().await);
+
+            drop(session);
+
+            assert_eq!(20, pool.idle().await);
+        })
+    }
+}
