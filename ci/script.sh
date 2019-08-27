41188051734c08b4896575fceeda0718711c169b
wolf4ood <enrico.risa@gmail.com>
2019-08-27 18:46:01 +0200

    added elif condition

--- ci/script.sh
+++ ci/script.sh
@@ -1,6 +1,5 @@
 if [[ $TRAVIS_RUST_VERSION == "stable" ]]; then
       cargo test --all
-fi
-if [[ $TRAVIS_RUST_VERSION == "nightly" ]]; then
+elif [[ $TRAVIS_RUST_VERSION == "nightly" ]]; then
       cargo test --all --all-features
 fi
