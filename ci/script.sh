90ce5046ec52e48dcebeef964f292dc5093d1706
wolf4ood <enrico.risa@gmail.com>
2019-08-27 18:53:05 +0200

    added --tests on stable

--- ci/script.sh
+++ ci/script.sh
@@ -1,5 +1,5 @@
 if [[ $TRAVIS_RUST_VERSION == "stable" ]]; then
-      cargo test --all
+      cargo test --all --tests
 elif [[ $TRAVIS_RUST_VERSION == "nightly" ]]; then
       cargo test --all --all-features
 fi
