c93d6d7b917d9dd4ef62ca99326b24bb44a3e942
Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
2019-10-10 12:34:42 +0200

    Add OSession with_retry test

--- orientdb-client/tests/orient-session.rs
+++ orientdb-client/tests/orient-session.rs
@@ -349,4 +349,17 @@ mod asynchronous {
             assert_eq!(3, result.len());
         })
     }
+
+    #[test]
+    fn session_query_test_with_retry() {
+        block_on(async {
+            let session = session("async_session_query_test_with_retry").await;
+            let result: Vec<_> = session.with_retry(10, |sess| {
+                sess.query("select from OUser")
+                    .page_size(1)
+            }).await.unwrap().collect().await;
+            
+            assert_eq!(3, result_len());
+        })
+    }
 }
