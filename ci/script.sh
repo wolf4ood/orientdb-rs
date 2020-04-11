8c0aca1733103363460886d9741971d2388d113f
wolf4ood <maggiolo00@gmail.com>
2020-04-11 20:01:01 +0200

    added changed dir in script

--- ci/script.sh
+++ ci/script.sh
@@ -1,3 +1,4 @@
+cd orientdb-client
 if [[ $TRAVIS_RUST_VERSION == "stable" ]]; then
       cargo test --all --features=async-std-runtime
       cargo test --all --features=tokio-runtime
@@ -5,3 +6,4 @@ elif [[ $TRAVIS_RUST_VERSION == "nightly" ]]; then
       cargo test --all --features=async-std-runtime
       cargo test --all --features=tokio-runtime
 fi
+cd ..
