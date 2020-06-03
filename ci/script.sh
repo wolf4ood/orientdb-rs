dbc3a23850d2a44bd539da77fffa7c7601dfdd23
wolf4ood <maggiolo00@gmail.com>
2020-06-03 15:35:47 +0200

    Added derive feature in CI

--- ci/script.sh
+++ ci/script.sh
@@ -1,9 +1,9 @@
 cd orientdb-client
 if [[ $TRAVIS_RUST_VERSION == "stable" ]]; then
-      cargo test --all --features=async-std-runtime,uuid
-      cargo test --all --features=tokio-runtime,uuid
+      cargo test --all --features=async-std-runtime,uuid,derive
+      cargo test --all --features=tokio-runtime,uuid,derive
 elif [[ $TRAVIS_RUST_VERSION == "nightly" ]]; then
-      cargo test --all --features=async-std-runtime,uuid
-      cargo test --all --features=tokio-runtime,uuid
+      cargo test --all --features=async-std-runtime,uuid,derive
+      cargo test --all --features=tokio-runtime,uuid,derive
 fi
 cd ..
