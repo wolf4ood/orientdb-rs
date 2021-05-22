0e9f352a674a18ea3f2a70d87ed69cc41a0e1624
wolf4ood <enrico.risa@gmail.com>
2021-05-22 23:09:16 +0200

    removed travis config

--- ci/script.sh
+++
@@ -1,9 +0,0 @@
-cd orientdb-client
-if [[ $TRAVIS_RUST_VERSION == "stable" ]]; then
-      cargo test --all --features=async-std-runtime,uuid,sugar
-      cargo test --all --features=tokio-runtime,uuid,sugar
-elif [[ $TRAVIS_RUST_VERSION == "nightly" ]]; then
-      cargo test --all --features=async-std-runtime,uuid,sugar
-      cargo test --all --features=tokio-runtime,uuid,sugar
-fi
-cd ..
